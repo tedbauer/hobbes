41a0d3d9ea6ecf78d47bcc12db8f8bd7f241b3c2
tedbauer <tjb272@cornell.edu>
2019-04-04 10:19:31 -0400

    Fix unary negation bug

--- src/parsetest.c
+++ src/parsetest.c
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
 {
 	if (argc == 1) {
 		int i;
-		for (i = 0; i < 51; i++) {
+		for (i = 0; i < NUM_FILES; i++) {
 			parse(files[i]);
 		}
 	} else if (argc == 2) {
