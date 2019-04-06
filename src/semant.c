0742c7ad0703a4864381df4fe093aa99419363f6
tedbauer <tjb272@cornell.edu>
2019-04-05 23:07:55 -0400

    Start type checking

---
+++ src/semant.c
@@ -0,0 +1,6 @@
+struct expTy(Tr_exp exp, Ty_ty ty) {
+	struct expty e;
+	e.exp = exp;
+	e.ty = ty;
+	return e;
+}
---
+++ src/semant.h
@@ -0,0 +1,7 @@
+struct expty { Tr_exp expl Ty_ty ty; };
+struct expty expTy(Tr_exp exp, Ty_ty ty);
+
+struct expty transVar(S_table venv, S_table tenv, A_var v);
+struct expty transExp(S_table venv, S_table tenv, A_exp a);
+void         transDec(S_table venv, S_table tenv, A_dec d);
+struct Ty_ty transTy (              S_table tenv, A_dec d);
